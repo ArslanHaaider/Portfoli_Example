53a7371ba3e79b6d0ac36cd024241659141c3e79
AslanHaaider <aslanhaider707@gmail.com>
2024-01-06 18:57:47 +0500

    Initialized baseline for the project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-slate-500 w-full h-screen'>
+      Hello! Tailwind is working for me!
+    </div>
+  )
+}
+
+export default App
