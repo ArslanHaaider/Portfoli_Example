dfd460167e934ca78066a2400bd040dbe28a1f75
AslanHaaider <aslanhaider707@gmail.com>
2024-01-06 20:18:45 +0500

    Created Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Nav from './components/nav'
 
 const App = () => {
   return (
-    <div className='bg-slate-500 w-full h-screen'>
-      Hello! Tailwind is working for me!
-    </div>
+    // <div className='bg-slate-500 w-full h-screen'>
+    //   Hello! Tailwind is working for me!
+    // </div>
+    <Nav/>
   )
 }
 
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import logo from '../assets/logo.png'   
+import { Link, NavLink } from 'react-router-dom';
+import { MagnifyingGlassIcon,HamburgerMenuIcon } from '@radix-ui/react-icons';
 const Nav = () => {
   return (
-    <div>
-        <img src="" alt="" />
+    <div className='border-orange-600 border-2 border-solid w-full h-24 flex items-center justify-between'>
+        <img className='ml-24' src={logo} alt="" />
+        <div className='flex items-center  w-60 h-3/5 justify-between mr-24'>
+            <a href="#" className='opacity-95 tracking-[.25em] uppercase text-sm'>Home</a>
+            <a href="#" className='opacity-30 tracking-[.25em] uppercase text-sm mr-5'>Contact</a>
+            <a href="#"><MagnifyingGlassIcon/></a>
+            <a href="#"><HamburgerMenuIcon/></a>
+        </div>
     </div>  
   )
 }
